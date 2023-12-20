2a0a7671a27303f026ab0f392399bd693d532ff5
Jaro <jaro.ambrozai@gmail.com>
2023-12-20 19:02:26 +0100

    video_02: added calendar

--- events/views.py
+++ events/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request):
-    return render(request, 'home.html', {})
+
+def home(request, year, month):
+    name = "Jaro"
+    month = month.capitalize()
+    # convert month from  name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    year = int(year)
+
+    # create calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    # get current year
+    now = datetime.now()
+    current_year = now.year
+
+    # get current time
+    current_time = now.strftime('%H:%M %p')
+    return render(request, 'home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "current_time": current_time,
+        })
